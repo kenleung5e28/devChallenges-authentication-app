2ed9409f806bd20de1e937abe6d9c6d7c500b497
Ken Leung <kenleung5e28@gmail.com>
2022-04-28 18:15:58 +0800

    update Login view

--- src/login/views/Login.tsx
+++ src/login/views/Login.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { SubmitHandler } from 'react-hook-form';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { LoginForm, LoginViewWrapper } from '@/login/components';
@@ -6,21 +7,18 @@ import { UserLoginInfo } from '@/login/types';
 import { auth } from '@/firebase';
 
 const Login: React.FC = () => {
-  const [signInWithEmailAndPassword, _, loading, error] = useSignInWithEmailAndPassword(auth);
+  const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
   const onSubmit: SubmitHandler<UserLoginInfo> = async ({ email, password }) => {
     await signInWithEmailAndPassword(email, password);
   };
 
-  if (loading) {
-    return (
-      <div>
-        <h2>Loading...</h2>
-      </div>
-    );
+  if (!loading && !error && user) {
+    return <Navigate to="/profile" />;
   }
 
   return (
     <LoginViewWrapper>
+      {loading && <div>Loading...</div>}
       {error && <div>Error: {error.message}</div>}
       <LoginForm
         title="Login"
