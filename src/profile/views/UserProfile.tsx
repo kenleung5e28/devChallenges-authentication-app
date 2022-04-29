c1981d515debb645172db689caab7d9a4e719e26
Ken Leung <kenleung5e28@gmail.com>
2022-04-29 09:18:25 +0800

    add a preliminary UserProfile view

--- src/profile/views/UserProfile.tsx
+++ src/profile/views/UserProfile.tsx
@@ -3,13 +3,31 @@ import { auth } from '@/firebase';
 
 const UserProfile: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
+
+  if (loading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <h2>Oops! Some problem occurred.</h2>
+        <p>{error.message}</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h3>
-        {displayName ?? 'N/A'}
-        {email ? `<${email}>` : ''}
+        {user?.displayName ?? 'N/A'}
+        {user?.email ? `<${user.email}>` : ''}
       </h3>
-      {photoURL && <img src={photoURL}></img>}
+      {user?.photoURL && <img src={user.photoURL}></img>}
     </div>
   );
 };
