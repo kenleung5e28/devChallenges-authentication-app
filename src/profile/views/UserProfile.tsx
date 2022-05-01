219e04bf60b36e7912ab712063458cf9e2ae89ab
Ken Leung <kenleung5e28@gmail.com>
2022-05-01 18:23:03 +0800

    remove redundant logic

--- src/profile/views/UserProfile.tsx
+++ src/profile/views/UserProfile.tsx
@@ -1,27 +1,8 @@
-import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/firebase';
 import { signOut } from 'firebase/auth';
 
 const UserProfile: React.FC = () => {
-  const [user, loading, error] = useAuthState(auth);
-
-  if (loading) {
-    return (
-      <div>
-        <h2>Loading...</h2>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div>
-        <h2>Oops! Some problem occurred.</h2>
-        <p>{error.message}</p>
-      </div>
-    );
-  }
-
+  const user = auth.currentUser;
   return (
     <div>
       <h3>
