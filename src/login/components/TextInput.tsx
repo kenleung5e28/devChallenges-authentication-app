781cd80e74c756b171a8f0e74aadd58e825932d8
Ken Leung <kenleung5e28@gmail.com>
2022-05-01 23:03:40 +0800

    add autocomplete attribute value

--- src/login/components/TextInput.tsx
+++ src/login/components/TextInput.tsx
@@ -1,35 +1,38 @@
-import styled from 'styled-components'
-import { RegisterOptions, useFormContext } from 'react-hook-form'
+import styled from 'styled-components';
+import { RegisterOptions, useFormContext } from 'react-hook-form';
 
 const Wrapper = styled.div`
-  border: 1px solid #BDBDBD;
+  border: 1px solid #bdbdbd;
   padding: 13px;
-  color: #BDBDBD;
-`
+  color: #bdbdbd;
+`;
 
 const InputBox = styled.input`
   border: none;
   width: 80%;
-`
+`;
 
 export interface TextInputProps {
-  name: string
-  label: string
-  isPassword?: boolean
-  options?: RegisterOptions
+  name: string;
+  label: string;
+  isPassword?: boolean;
+  options?: RegisterOptions;
 }
 
 const TextInput: React.FC<TextInputProps> = ({ name, label, isPassword = false, options }) => {
-  const { register } = useFormContext()
-  return <Wrapper>
-    <label>
-      <InputBox
-        type={isPassword ? 'password' : 'text'}
-        placeholder={label}
-        {...register(name, options)}
-      />
-    </label>
-  </Wrapper>
-}
+  const { register } = useFormContext();
+  return (
+    <Wrapper>
+      <label>
+        <InputBox
+          autoComplete="on"
+          type={isPassword ? 'password' : 'text'}
+          placeholder={label}
+          {...register(name, options)}
+        />
+      </label>
+    </Wrapper>
+  );
+};
 
-export default TextInput
+export default TextInput;
